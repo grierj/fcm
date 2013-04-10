7c2de944a7fbe3975f5ee80f8532c9c637d85879
Grier Johnson <grier@squareup.com>
2013-04-10 10:46:09 -0700

    Initial swipe at C based agent Using APR pools

---
+++ src/fcm-agent-main.c
@@ -0,0 +1,193 @@
+/*
+ * A C implementation of the perl fcm-agent
+ */
+
+#include "fcm.h"
+
+int main(int argc, char *argv[])
+{
+  // Let's talk variables!
+  fcm_opts_t *opts = NULL;
+  struct stat ad;
+  struct stat dd;
+  apr_pool_t *pool;
+  apr_file_t *out;
+  apr_file_t *err;
+  int c;
+  char full_path[PATH_MAX];
+
+  // Initialize apr
+  apr_initialize();
+  atexit(apr_terminate);
+  // delicious memory pool
+  apr_pool_create(&pool, NULL);
+  // Set up printing to the console
+  apr_file_open_stdout(&out, pool);
+  apr_file_open_stderr(&err, pool);
+
+  //get our opts going
+  opts = apr_palloc(pool, sizeof(fcm_opts_t));
+
+  // Set up some defaults that we will blow away with command line args
+  // if need be
+  opts->agent_dir = apr_pstrdup(pool, "/var/fcm/agent");
+  opts->data_dir = apr_pstrdup(pool, "/var/fcm/data");
+  opts->sleep_time = 600;
+  opts->verbose = 0;
+  opts->run_once = 0;
+  opts->out = out;
+  opts->err = err;
+  opts->pool = pool;
+
+  //TODO(grier): Move arg parsing to its own function
+  // -a = agent dir
+  // -d = data dir
+  // -h = help
+  // -s = sleep time
+  // -v = verbose
+  while ((c = getopt (argc, argv, "a:d:hos:v")) != -1)
+    switch (c)
+    {
+      case 'a':
+        realpath(optarg, full_path);
+        opts->agent_dir = apr_pmemdup(pool, full_path, sizeof(full_path));
+        break;
+      case 'd':
+        realpath(optarg, full_path);
+        opts->data_dir = apr_pmemdup(pool, full_path, sizeof(full_path));
+        break;
+      case 'h':
+        apr_file_printf(out, "Usage: fcm-agent [adhsv]\n");
+        apr_file_printf(out, "-a: Agent directory\n");
+        apr_file_printf(out, "-d: Data directory\n");
+        apr_file_printf(out, "-s: sleep time between runs\n");
+        apr_file_printf(out, "-v: print verbose messages\n");
+        apr_file_printf(out, "-h: this help message\n");
+        return 0;
+      case 'o':
+        opts->run_once = 1;
+        break;
+      case 's':
+        opts->sleep_time = atoi(optarg);
+        break;
+      case 'v':
+        opts->verbose = 1;
+        break;
+      case '?':
+        if (optopt == 'a' || optopt == 'd' || optopt == 's')
+          apr_file_printf(err, "Option -%a requires and argument.\n", optopt);
+        else if (isprint (optopt))
+          apr_file_printf(err, "Unknown option -%c\n", optopt);
+        else
+          apr_file_printf(err, "Unknown options character \\x%x\n", optopt);
+        return 1;
+      default:
+        abort();
+    }
+
+  if(stat(opts->agent_dir, &ad) != 0 || ! S_ISDIR(ad.st_mode))
+  {
+    apr_file_printf(err, "Agent directory does not exist: %s\n", opts->agent_dir);
+    return 1;
+  }
+  else if(stat(opts->data_dir, &dd) != 0 || ! S_ISDIR(ad.st_mode))
+  {
+    apr_file_printf(err, "Data directory does not exist: %s\n", opts->data_dir);
+    return 1;
+  }
+
+  agent_loop(opts);
+
+  return 0;
+}
+
+void agent_loop(fcm_opts_t *opts)
+{
+  DIR *dir_h;
+  struct dirent *file;
+  struct stat *af;
+  struct stat *df;
+  char *agent_file = NULL;
+  char *data_file = NULL;
+  char *module_name = NULL;
+  apr_hash_t *pid_map = NULL;
+  pid_t cpid;
+  int mod_pid;
+  int cstatus;
+
+  dir_h = apr_palloc(opts->pool, sizeof(dir_h));
+  file = apr_palloc(opts->pool, sizeof(struct dirent));
+  af = apr_palloc(opts->pool, sizeof(struct stat));
+  df = apr_palloc(opts->pool, sizeof(struct stat));
+  agent_file = apr_palloc(opts->pool, PATH_MAX);
+  data_file = apr_palloc(opts->pool, PATH_MAX);
+  module_name = apr_palloc(opts->pool, PATH_MAX);
+  pid_map = apr_hash_make(opts->pool);
+
+  while(1)
+  {
+    apr_file_printf(opts->out, "\nWaking up for run\n");
+    dir_h = opendir(opts->agent_dir);
+    while (dir_h)
+    {
+      if ((file = readdir(dir_h)) != NULL)
+      {
+        if (apr_strnatcmp(file->d_name, ".") == 0 || apr_strnatcmp(file->d_name, "..") == 0)
+        {
+          if (opts->verbose) apr_file_printf(opts->out, "Skipping: %s/%s\n", opts->agent_dir, file->d_name);
+          continue;
+        }
+        else
+        {
+          if (opts->verbose) apr_file_printf(opts->out, "Found: %s/%s\n", opts->agent_dir, file->d_name);
+          agent_file = apr_psprintf(opts->pool, "%s/%s", opts->agent_dir, file->d_name);
+          data_file = apr_psprintf(opts->pool, "%s/%s", opts->data_dir, file->d_name);
+          if (stat(agent_file, af) == 0 && S_ISREG(af->st_mode) && stat(data_file, df) == 0 && S_ISREG(df->st_mode))
+          {
+            apr_file_printf(opts->out, "Running: %s %s\n", agent_file, data_file);
+            mod_pid = run_module(opts, agent_file, data_file);
+            apr_hash_set(pid_map, apr_itoa(opts->pool, mod_pid), APR_HASH_KEY_STRING, agent_file);
+          }
+          else
+          {
+            if (opts->verbose) apr_file_printf(opts->out, "Skipping (not a file): %s/%s\n", opts->agent_dir, file->d_name);
+          }
+        }
+      }
+      else
+      {
+        closedir(dir_h);
+        break;
+      }
+    }
+    //TODO(grier): Add process name to output here
+    while ((cpid = wait(&cstatus)) > 0)
+    {
+      module_name = apr_hash_get(pid_map, apr_itoa(opts->pool, cpid), APR_HASH_KEY_STRING);
+      apr_file_printf(opts->out, "Child %s (%ld) terminated with ret code %i\n", module_name, cpid, cstatus);
+    }
+
+    apr_file_printf(opts->out, "Loop end, sleeping for %i\n", opts->sleep_time);
+    if (opts->run_once) break;
+    sleep(opts->sleep_time);
+  }
+}
+
+int run_module(fcm_opts_t *opts, char *agent_file, char *data_file)
+{
+  pid_t pID = fork();
+
+  if (pID == 0)
+  {
+    if (opts->verbose) apr_file_printf(opts->err, "Child to exec '%s %s'\n", agent_file, data_file);
+    execl(agent_file, agent_file, data_file, (char *) 0);
+    apr_file_printf(opts->err, "Failed to exec '%s %s'\n", agent_file, data_file);
+    return 0;
+  }
+  else if (pID < 0)
+  {
+    apr_file_printf(opts->err, "Failed to fork '%s %s'\n", agent_file, data_file);
+    return 0;
+  }
+  return pID;
+}
